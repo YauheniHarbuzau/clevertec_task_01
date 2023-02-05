3ff5d815b7fdda630c329e4eabcbaba1f0b7e036
YauheniHarbuzau <rimestorm7@gmail.com>
2023-02-05 15:27:47 +0300

    showAnimal() method in Main class

--- src/edu/clever/clevertec_task_01/Main.java
+++ src/edu/clever/clevertec_task_01/Main.java
@@ -3,15 +3,20 @@ package edu.clever.clevertec_task_01;
 import edu.clever.clevertec_task_01.entity.Cat;
 import edu.clever.clevertec_task_01.entity.Dog;
 import edu.clever.clevertec_task_01.entity.Fish;
+import edu.clever.clevertec_task_01.entity.parent.Animal;
 
 public class Main {
 
     public static void main(String[] args) {
+        showAnimal(new Cat("Борис"));
+        showAnimal(new Dog("Полкан"));
+        showAnimal(new Fish("Салака"));
+    }
 
-        System.out.println(new Cat("Борис"));
-
-        System.out.println(new Dog("Полкан"));
-
-        System.out.println(new Fish("Салака"));
+    public static void showAnimal(Animal animal) {
+        System.out.println(animal);
+        animal.breathe();
+        animal.eating();
+        System.out.println("----------");
     }
 }
