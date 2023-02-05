012aacc14981caebcfdf249b4e8672b38f85e755
YauheniHarbuzau <rimestorm7@gmail.com>
2023-02-05 13:47:25 +0300

    creation Cat class

--- src/edu/clever/clevertec_task_01/Main.java
+++ src/edu/clever/clevertec_task_01/Main.java
@@ -1,7 +1,13 @@
 package edu.clever.clevertec_task_01;
 
+import edu.clever.clevertec_task_01.entity.Cat;
+import edu.clever.clevertec_task_01.entity.parent.Animal;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Animal cat = new Cat("Борис");
+        System.out.println(cat);
     }
 }
