02a5666d03357580b45cc09294a38d08d57587d8
YauheniHarbuzau <rimestorm7@gmail.com>
2023-02-05 15:11:24 +0300

    creation interfaces Breathe and Eating

---
+++ src/edu/clever/clevertec_task_01/entity/parent/Breathe.java
@@ -0,0 +1,6 @@
+package edu.clever.clevertec_task_01.entity.parent;
+
+public interface Breathe {
+
+    void breathe();
+}
---
+++ src/edu/clever/clevertec_task_01/entity/parent/Eating.java
@@ -0,0 +1,6 @@
+package edu.clever.clevertec_task_01.entity.parent;
+
+public interface Eating {
+
+    void eating();
+}
