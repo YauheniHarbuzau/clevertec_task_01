98b065f303142e380b9c4b82602ec2a0db4c8790
YauheniHarbuzau <rimestorm7@gmail.com>
2023-02-05 14:58:25 +0300

    creation animal entity in Main class

--- src/edu/clever/clevertec_task_01/Main.java
+++ src/edu/clever/clevertec_task_01/Main.java
@@ -1,9 +1,23 @@
 package edu.clever.clevertec_task_01;
 
+import edu.clever.clevertec_task_01.entity.Cat;
+import edu.clever.clevertec_task_01.entity.Dog;
+import edu.clever.clevertec_task_01.entity.parent.Animal;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // here are some fixes in Main class
+        Animal cat1 = new Cat("Борис");
+        System.out.println(cat1);
+
+        Animal cat2 = new Cat("Мурка");
+        System.out.println(cat2);
+
+        Animal dog1 = new Dog("Полкан");
+        System.out.println(dog1);
+
+        Animal dog2 = new Dog("Рекс");
+        System.out.println(dog2);
     }
 }
